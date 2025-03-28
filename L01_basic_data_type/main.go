bcf185a6859386d9ba7f2c8c95389fad47e076ef
Charlie871001 <mahui871001@gmail.com>
2025-03-28 16:57:37 +0800

    'basic'

--- L01_basic_data_type/main.go
+++ L01_basic_data_type/main.go
@@ -32,6 +32,8 @@ func main() {
 	var s string = "Hello, world!"
 	var bytes []byte = []byte(s)
 	fmt.Println("convert \"Hello, world!\" to bytes: ", bytes)
+	fmt.Println("string length:", len(s))
+	fmt.Println("bytes length:", len(bytes))
 
 	// 同时[]byte 也可以直接转换成 string。
 	var bytes1 []byte = []byte{72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100, 33}
@@ -47,5 +49,29 @@ func main() {
 	var s2 string = "Hello, world!"
 	var runes []rune = []rune(s2)
 	fmt.Println("convert \"Hello, world!\" to runes: ", runes)
+	fmt.Println("runes length:", len(runes))
+
+	// 在 Go 中，字符串是 UTF-8 编码的，并且所有的 Go 源码都必须是 UTF-8 编码。
+	var s3 string = "Hello\nworld!\n"
+	var s4 string = `Hello
+world!
+`
+	fmt.Println(s3 == s4)
+
+	var s0 string = "Go语言"
+	var bytes0 []byte = []byte(s0)
+	var runes0 []rune = []rune(s0)
+
+	fmt.Println("string length: ", len(s0))
+	fmt.Println("bytes length: ", len(bytes0), bytes0)
+	fmt.Println("runes length: ", len(runes0), runes0)
+
+	var s5 string = "Go语言"
+	var bytes5 []byte = []byte(s5)
+	var runes5 []rune = []rune(s5)
+
+	fmt.Println("string sub: ", s5[0:7])
+	fmt.Println("bytes sub: ", string(bytes5[0:7]))
+	fmt.Println("runes sub: ", string(runes5[0:3]))
 
 }
