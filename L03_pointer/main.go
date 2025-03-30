d51349f5efc75af4834029ccbbca836587978d4d
Charlie871001 <mahui871001@gmail.com>
2025-03-30 15:27:36 +0800

    pointer

---
+++ L03_pointer/main.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+	// var p1 *int
+	// var p2 *string
+	// i := 2
+	// s := "hello"
+
+	// p1 = &i
+	// p2 = &s
+
+	// p3 := &p2
+	// fmt.Println(p1, p2, p3)
+
+	// var p1 *int
+	// i := 2
+	// p1 = &i
+	// fmt.Println(*p1 == i)
+	// *p1 = 3
+	// fmt.Println(i)
+}
